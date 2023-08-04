ba7aac4a2450d60f91af6de907ab4a4d1bbcaa39
Eli <eli.serra173@gmail.com>
2023-08-04 04:13:33 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,45 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+setup(
+    name='imgtools_m8',
+    version='0.0.1',
+    packages=['tests', 'imgtools_m8'],
+    url='https://github.com/mano8/imgtools_m8',
+    license='Apache',
+    author='Eli Serra',
+    author_email='eli.serra173@gmail.com',
+    description='Simple image tools package. Used to convert, downscale or upscale images.',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    classifiers=[
+            "License :: OSI Approved :: Apache License",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.5",
+            "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: 3.7",
+            "Programming Language :: Python :: 3.8",
+            "Programming Language :: Python :: 3.9",
+            "Programming Language :: Python :: 3.10",
+      ],
+    include_package_data=True,
+    install_requires=[
+          'opencv-contrib-python>=4.7.0.72',
+          've_utils>=2.5.3',
+          'numpy>=1.25.0'
+      ],
+    extras_require={
+              "TEST": [
+                    "pytest>=7.1.2",
+                    "coverage"
+              ]
+      },
+    python_requires='>3.5.2',
+    zip_safe=False
+)
