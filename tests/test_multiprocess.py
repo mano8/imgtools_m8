2288f6c230426c42ed778de1e66cebd499c86d89
Eli <eli.serra173@gmail.com>
2023-08-10 00:46:40 +0200

    add MultiProcessImage unitest class

---
+++ tests/test_multiprocess.py
@@ -0,0 +1,61 @@
+"""
+ImageTools unittest class.
+
+Use pytest package.
+"""
+from os import path
+from .helper import HelperTest
+from imgtools_m8.multiprocess import MultiProcessImage
+
+__author__ = "Eli Serra"
+__copyright__ = "Copyright 2020, Eli Serra"
+__deprecated__ = False
+__license__ = "MIT"
+__status__ = "Production"
+__version__ = "1.0.0"
+
+
+class TestImageTools:
+
+    def setup_method(self):
+        """
+        Setup any state tied to the execution of the given function.
+
+        Invoked for every test function in the module.
+        """
+        source_path = HelperTest.get_source_path()
+        output_conf = {
+            'path': HelperTest.get_output_path(),
+            'output_formats': [
+                {
+                    'fixed_width': 260,
+                    'fixed_height': 200,
+                    'formats': [
+                        {'ext': '.jpg', 'quality': 80}
+                    ]
+                }
+            ]
+
+        }
+
+        self.obj = MultiProcessImage(
+            source_path=source_path,
+            output_conf=output_conf
+        )
+
+    def test_run_multiple(self):
+        """Test run_multiple method"""
+        tst = self.obj.run_multiple()
+        # unable to upscale bad_image.jpg
+        assert tst is False
+
+        self.obj.set_source_path(
+            source_path=path.join(
+                HelperTest.get_source_path(),
+                'good'
+            )
+        )
+
+        tst = self.obj.run_multiple()
+        # unable to upscale bad_image.jpg
+        assert tst is True
