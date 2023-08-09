b91f6d24c4d1cda05c9378a29503d12d87825ffa
Eli <eli.serra173@gmail.com>
2023-08-09 09:30:53 +0200

    update package version to v0.0.4

--- imgtools_m8/__init__.py
+++ imgtools_m8/__init__.py
@@ -6,7 +6,7 @@ __copyright__ = "Copyright 2023, Eli Serra"
 __deprecated__ = False
 __license__ = "MIT"
 __status__ = "Production"
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 
 
 class AppFilter(logging.Filter):
