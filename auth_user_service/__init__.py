0dafd5893363b298b3768e1990ad828ed87b5b3f
Eli <eli.serra173@gmail.com>
2026-05-19 08:34:56 +0200

    chore: update version to 0.7.3 in auth_user_service and fastapi_service

--- auth_user_service/__init__.py
+++ auth_user_service/__init__.py
@@ -1,5 +1,5 @@
 """auth_user_service fastapi app"""
 
-__version__ = "0.7.2"
+__version__ = "0.7.3"
 
 __all__ = ["__version__"]
--- examples/fastapi_service/__init__.py
+++ examples/fastapi_service/__init__.py
@@ -1,5 +1,5 @@
 """fastapi_service fastapi app"""
 
-__version__ = "0.7.2"
+__version__ = "0.7.3"
 
 __all__ = ["__version__"]
